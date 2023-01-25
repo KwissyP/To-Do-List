df456713158229a3b58cc4b48ec3f902e651b04d
KwissyP <christianpandoulas@hotmail.com>
2023-01-25 19:51:49 +0100

    Done

--- public/js/app.js
+++ public/js/app.js
@@ -11,88 +11,72 @@ form.addEventListener("submit", (e) => {
     newTask.classList.add("code");
     newTask.classList.add("shadow-sm");
     newTask.setAttribute("draggable", "true");
-    newTask.addEventListener('dragstart', function () {
-        draggedItem = newTask;
-        setTimeout(function () {
-            newTask.style.display = 'none';
-        }, 0)
+    newTask.addEventListener("dragstart", () => {
+        newTask.classList.add("is-dragging");
     });
-    newTask.addEventListener('dragend', function () {
-        setTimeout(function () {
-            newTask.style.display = 'block';
-            draggedItem = null;
-        }, 0);
-        draggedItem = null;
-    });
-    newTask.addEventListener('dragover', function (e) {
-        e.preventDefault();
-    });
-
-    newTask.addEventListener('dragenter', function (e) {
-        e.preventDefault();
-    });
-
-    newTask.addEventListener('drop', function () {
-        console.log('drop');
-        this.append(draggedItem);
+    newTask.addEventListener("dragend", () => {
+        newTask.classList.remove("is-dragging");
     });
     newTask.innerText = value.toUpperCase();
     colOne.appendChild(newTask);
     input.value = "";
 });
 
-// Drag & Drop
-const code = document.querySelectorAll('.code');
-const col = document.querySelectorAll('.col');
+const draggables = document.querySelectorAll(".code");
+const droppables = document.querySelectorAll(".col");
 
-let draggedItem = null;
+// Selection de tous les éléments avec la classe "code" pour les rendre draggable
+draggables.forEach((task) => {
+    task.setAttribute("draggable", 'true');
+    // Ajout d'un écouteur d'événement "dragstart" pour ajouter la classe "is-dragging"
+    task.addEventListener("dragstart", () => {
+        task.classList.add("is-dragging");
+    });
+    // Ajout d'un écouteur d'événement "dragend" pour enlever la classe "is-dragging"
+    task.addEventListener("dragend", () => {
+        task.classList.remove("is-dragging");
+    });
+});
 
-for (let i = 0; i < code.length; i++) {
-    let item = code[i];
-    item.setAttribute('draggable', 'true');
+// Selection de tous les éléments avec la classe "col" pour les rendre droppable
+droppables.forEach((zone) => {
+    // Ajout d'un écouteur d'événement "dragover" pour autoriser le drop
+    zone.addEventListener("dragover", (e) => {
+        e.preventDefault();
 
-    item.addEventListener('dragstart', function () {
-        draggedItem = item;
-        setTimeout(function () {
-            item.style.display = 'none';
-        }, 0)
+        // utilisation de la fonction pour insérer l'élément draggable au dessus de l'élement code le plus proche
+        const bottomTask = insertAboveTask(zone, e.clientY);
+        const curTask = document.querySelector(".is-dragging");
+
+        // Si pas d'élement code trouvé, on append l'élément draggable
+        if (!bottomTask) {
+            zone.appendChild(curTask);
+        } else {
+            // Sinon on insère l'élément draggable avant l'élement code trouvé
+            zone.insertBefore(curTask, bottomTask);
+        }
+    });
+});
+
+// Fonction pour trouver l'élement code le plus proche de l'emplacement ou l'élement draggable est laché
+const insertAboveTask = (zone, mouseY) => {
+    const els = zone.querySelectorAll(".code:not(.is-dragging)");
+
+    let closestTask = null;
+    let closestOffset = Number.NEGATIVE_INFINITY;
+
+    els.forEach((task) => {
+        const {
+            top
+        } = task.getBoundingClientRect();
+
+        const offset = mouseY - top;
+
+        if (offset < 0 && offset > closestOffset) {
+            closestOffset = offset;
+            closestTask = task;
+        }
     });
 
-    item.addEventListener('dragend', function () {
-        setTimeout(function () {
-            item.style.display = 'block';
-            draggedItem = null;
-        }, 0);
-        draggedItem = null;
-    })
-
-    for (let j = 0; j < col.length; j++) {
-        let list = col[j];
-
-        list.addEventListener('dragover', function (e) {
-            e.preventDefault();
-        });
-
-        list.addEventListener('dragenter', function (e) {
-            e.preventDefault();
-        });
-
-        list.addEventListener('drop', function (e) {
-            const target = e.target;
-
-            if (target && target !== draggedItem && target.classList.contains('code')) {
-                const nextSibling = target.nextElementSibling;
-                const parent = target.parentNode;
-                parent.insertBefore(draggedItem, target);
-                if (nextSibling) {
-                    parent.insertBefore(target, nextSibling);
-                } else {
-                    parent.appendChild(target);
-                    target.nextElementSibling = nextSibling;
-                }
-            } else {
-                list.appendChild(draggedItem);
-            }
-        });
-    }
-}
+    return closestTask;
+};
